abaf94638c98f8a386cdda223d98b4293828f5f9
lpicanco <lpicanco@gmail.com>
2025-08-15 16:27:54 +0100

    perf: use uds

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod model;
 mod workers;
 
 use std::env;
+use std::os::unix::fs::PermissionsExt;
 
 use crate::handlers::{create_payment, get_payments_summary, reset_handler};
 use crate::model::App;
@@ -13,6 +14,8 @@ use axum::routing::post;
 use axum::{Router, routing::get};
 use deadpool_redis::{Config, Runtime};
 use log::info;
+use tokio::net::UnixListener;
+use tokio::signal;
 
 #[tokio::main]
 async fn main() {
@@ -23,15 +26,18 @@ async fn main() {
     }
     env_logger::init();
 
-    let port = env::var("PORT").unwrap_or("9999".to_string());
-    let redis_url = env::var("REDIS_URL").unwrap_or("redis://localhost:6379".to_string());
-    let cfg = Config::from_url(redis_url);
-    let redis_pool = cfg.create_pool(Some(Runtime::Tokio1)).unwrap();
+    let uds_path = env::var("UDS_PATH").unwrap_or("/tmp/uds42".to_string());
+    let redis_url = env::var("REDIS_URL").unwrap_or("redis://dev-server:6379".to_string());
+    let redis_pool = Config::from_url(redis_url)
+        .builder().unwrap()
+        .max_size(20)
+        .runtime(Runtime::Tokio1)
+        .build().unwrap();
 
     let payment_endpoint =
-        env::var("PAYMENT_ENDPOINT").unwrap_or("http://localhost:8001".to_string());
+        env::var("PAYMENT_ENDPOINT").unwrap_or("http://dev-server:8001".to_string());
     let payment_fallback_endpoint =
-        env::var("PAYMENT_FALLBACK_ENDPOINT").unwrap_or("http://localhost:8002".to_string());
+        env::var("PAYMENT_FALLBACK_ENDPOINT").unwrap_or("http://dev-server:8002".to_string());
 
     let app_state = App::new(redis_pool, payment_endpoint, payment_fallback_endpoint);
 
@@ -51,10 +57,41 @@ async fn main() {
         .route("/purge-payments", post(reset_handler::handle))
         .with_state(app_state);
 
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
+    std::fs::remove_file(uds_path.clone()).ok();
+    let listener = UnixListener::bind(uds_path.clone()).unwrap();
+
+    let mut perms = std::fs::metadata(&uds_path).unwrap().permissions();
+    perms.set_mode(0o666);
+    std::fs::set_permissions(&uds_path, perms).unwrap();
+
+    info!("🦀backend running at http://localhost:{}/", uds_path);
+
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
+}
 
-    info!("🦀backend running at http://localhost:{}/", port);
-    axum::serve(listener, app).await.unwrap();
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
